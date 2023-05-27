d077bd158366797143d1b62c76368a2cf7107f00
esraaamaireh <esraa999amayreh@gmail.com>
2023-05-27 23:29:49 +0000

    I updat for lap10

--- js/apps.js
+++ js/apps.js
@@ -3,7 +3,8 @@
 var num = 1;
 var n; var netsalary;
 var sum = 0;
-const earr = [];
+
+const allEmployee = [];
 
 function Employee(fullName, department, level, imageurl1) {
     this.fullName = fullName;
@@ -11,23 +12,53 @@ function Employee(fullName, department, level, imageurl1) {
     this.level = level;
 
 
-    this.imageurl = imageurl1;
-    earr.push(this);
-    console.log(earr);
+    this.imageurl1 = imageurl1;
+   
+    allEmployee.push(this);
 
 
-}
+};
 Employee.prototype.render = function () {
-    document.write(`</br> </br> </br> <div class ="data" :>Employee name :  ${ghazi.fullName}  Department: ${ghazi.department} Employee salary : ${ghazi.salary('senior')}    <\div></br> 
-    <div class ="data" :> Employee name :  ${lana.fullName}  Department: ${lana.department}. Employee salary : ${lana.salary('senior')}<\div></br>
- </br>   <div class ="data" :> Employee name :  ${tamara.fullName}  Department: ${tamara.department}. Employee salary : ${tamara.salary('senior')}<\div></br>
-  </br> <div class ="data" :>  Employee name :  ${safi.fullName}  Department: ${safi.department}. Employee salary : ${safi.salary('senior')}<\div></br>
-   </br><div class ="data" :>  Employee name : ${omar.fullName}  Department: ${omar.department}. Employee salary : ${omar.salary('senior')}<\div></br>
- </br> <div class ="data" :>   Employee name :  ${rana.fullName}  Department: ${rana.department}. Employee salary : ${rana.salary('senior')}<\div></br>
- </br>  <div class ="data" :>  Employee name :  ${hadi.fullName}  Department: ${hadi.department}. Employee salary : ${hadi.salary('senior')}<div></br>`);
 
-}
 
+    let divE = document.createElement("div");
+    let main = document.getElementsByClassName("main1")[0];
+  
+
+    main.appendChild(divE);
+
+    let imgE = document.createElement("img");
+    imgE.setAttribute("src", this.imageurl1);
+    imgE.style.width = "300px";
+    imgE.style.height = "300px";
+    divE.appendChild(imgE);
+let employeeName = document.createElement("h4");
+    employeeName.textContent = ` Name : ${this.fullName}`;
+    divE.appendChild(employeeName);
+    let employeeId = document.createElement("h4");
+    employeeId.textContent = `ID: ${this.employeeID()}`;
+    divE.appendChild(employeeId);
+    let departMent = document.createElement("h4");
+    departMent.textContent = ` Department : ${this.department} `;
+    divE.appendChild(departMent); 
+    let eLevel = document.createElement("h4");
+    eLevel.textContent = ` Level  : ${this.level} `;
+    divE.appendChild(eLevel);  
+    let eSalary = document.createElement("h4");
+    eSalary.textContent = ` Salary : ${this.salary()}`;
+    divE.appendChild(eSalary);
+
+
+//styling 
+    divE.style.border = "1px solid";
+    divE.style.borderRadius ="10px"
+        divE.style.backgroundColor = "FFF8DE";
+    divE.style.alignContent = "inline";
+    divE.style.justifyItems="space-evenly;"
+ 
+    
+
+};
 Employee.prototype.employeeID = function () {
     var string = "" + num;
     var pad = "0000";
@@ -35,42 +66,43 @@ Employee.prototype.employeeID = function () {
     num++;
     return n;
 
-}
-Employee.prototype.salary = function (level) {
-    if (level == 'senior')
+};
+Employee.prototype.salary = function () {
+    if (this.level == 'senior')
 
         return getRandomIdForSalary(1500, 2000)
-    else if (level == 'mid-senior')
+    else if (this.level == 'mid-senior')
 
         return getRandomIdForSalary(1000, 1500)
 
-    else if (level == 'jenior')
+    else if (this.level == 'jenior')
         return getRandomIdForSalary(500, 1000)
 
-}
-    ;
-var ghazi = new Employee('Ghazi Samer', 'Administration', 'senior', 'ghazi photo ');
+};
 
-console.log(ghazi.employeeID(n));
 
-let lana = new Employee('Lana Ali', 'Finance', 'senior', 'lana photo ');
-
-console.log('Emolyee id is ' + lana.employeeID(n));
-
-let tamara = new Employee(' Tamara Ayoub ', 'Marketing', 'senior', 'tamara photo ');
-console.log('Emolyee id is ' + tamara.employeeID(n));
-
-let safi = new Employee(' Safi Walid ', 'Administration', 'mid-senior', 'safi photo ');
-console.log('Emolyee id is ' + safi.employeeID(n));
-let omar = new Employee('Omar Zaid', 'Development', 'senior', 'omar photo ');
-console.log('Emolyee id is ' + omar.employeeID(n));
-let rana = new Employee(' Rana Saleh   ', 'Development', 'jenior', 'rana photo ');
-console.log('Emolyee id is ' + rana.employeeID(n) + 'employee salary ' + rana.salary('jenior'));
-let hadi = new Employee(' Hadi Ahmad    ', 'Finance', 'senior', 'hadi photo ');
-console.log('Emolyee id is ' + hadi.employeeID(n));
+let ghazi = new Employee(
+    'Ghazi Samer',
+    'Administration',
+    'senior',
+    "./asset/Ghazi.jpg");
 
 
 
+let lana = new Employee('Lana Ali', 'Finance', 'senior', "./asset/Lana.jpg");
+
+
+
+let tamara = new Employee(' Tamara Ayoub ', 'Marketing', 'senior', "./asset/Tamara.jpg");
+
+
+let safi = new Employee(' Safi Walid ', 'Administration', 'mid-senior', "./asset/Safi.jpg");
+
+let omar = new Employee('Omar Zaid', 'Development', 'senior', "./asset/Omar.jpg");
+
+let rana = new Employee(' Rana Saleh   ', 'Development', 'jenior', "./asset/Rana.jpg");
+
+let hadi = new Employee(' Hadi Ahmad    ', 'Finance', 'senior', "./asset/Hadi.jpg");
 
 
 
@@ -98,4 +130,6 @@ function netSalary(net) {
 } console.log("The net salary of all employees is  " + netSalary(net))
 
 
-ghazi.render();
+for (let i = 0; i < allEmployee.length; i++) {
+    allEmployee[i].render();
+};
